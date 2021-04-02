fe5690b812445b97cd996a3eb49ce6dbc6f4bd1b
Robert Tilton <robtilton2086@gmail.com>
2021-04-02 14:54:27 -0400

    adding more file structure

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <h1>Employee Directory</h1>
+                <p>Input name in search field to search a specific employee!</p>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
